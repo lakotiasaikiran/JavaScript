bbae211cd43ba42c44066d13ac058d705c3e41fd
L. SAI KIRAN <saikiranlakotia@gmail.com>
2026-09-18 10:20:21 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accId = 12345
+let accEmail = "hello@gmail.com"
+var accPass = "qwerty"
+accCity = "Hyd"
+
+// accId = 2
+accEmail = "hi@gmail.com"
+accPass = "asdfghj"
+accCity = "Mumbai"
+console.log(accId);
+
+/* Don't use var 
+bcz of issue in block scope and functional scope 
+*/
+
+console.log([accId, accEmail, accPass, accCity])
